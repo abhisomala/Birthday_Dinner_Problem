aa5c0d5e5ef68332e84daa864a16779cd21730c5
asomala <68791501+abhisomala@users.noreply.github.com>
2022-02-01 14:47:08 +0000

    Small changes with question and how the tax and tip is calculated

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ while True:
   #Makes sure the code runs forever
   try:  #prevents any errors
     
-    owedbefore = int(input("\nInput how much that person owes before tip and tax: "))
+    owedbefore = int(input("\nInput how much that person owes before tax and tip:  "))
     #Takes input
     if owedbefore == 10 or owedbefore == 12 or owedbefore == 9 or owedbefore == 8 or owedbefore == 7 or owedbefore == 15 or owedbefore == 11 or owedbefore == 30:  
       #Only calculates if anyone owed that amount of money
-      owedafter = owedbefore * 1.5 * 1.15 
+      owedafter = (owedbefore * 1.5) + (owedbefore * 0.15) 
       #Calculates tip and tax
       rowedafter = "{:.2f}".format(owedafter)
       #Prevents any decimals going past the hundrets place
-      print("That person owes $", rowedafter, "after tax an tip")
+      print("That person owes $", rowedafter, "after tax and tip")
       #Tells user the amount owed
     else:
       print("No one owed that amount of money!")
